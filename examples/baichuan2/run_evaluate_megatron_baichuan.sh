88cf35f8e928e21885ecd2ff470f250bafcdd054
Jerry Li <jerryli1981@users.noreply.github.com>
2023-09-19 10:31:37 +0800

    Add Baichuan-2 for 7B model (#22)
    * Fix train/valid data nargs in arguments
    
    * Update Readme
    
    * gpt3_llama convertor support transformer engine
    
    * Add Fused RMSNorm for llama and gpt_llama
    
    * Add Baichuan-2 for 7B model
    
    ---------
    
    Co-authored-by: 同润 <jerry.lp@alibaba-inc.com>

---
+++ examples/baichuan2/run_evaluate_megatron_baichuan.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+# sh run_evaluate_megatron_baichuan.sh dsw /workspace/Megatron-LM-main/ /workspace/github/Pai-Megatron-Patch/ 7B 1 2048 2048 0 bf16 1 1 sel true false false false /mnt/baichuan2-datasets/alpaca_zh.json /mnt/baichuan2-ckpts/Baichuan2-7B-Base-to-mg-tp1-pp1
+# sh run_evaluate_megatron_baichuan.sh dsw /workspace/Megatron-LM-main/ /workspace/github/Pai-Megatron-Patch/ 13B 1 2048 2048 0 bf16 2 1 sel true false false false /mnt/baichuan2-datasets/alpaca_zh.json /mnt/baichuan2-ckpts/Baichuan2-13B-Base-to-mg-tp2-pp1
+
+set -e
+ENV=$1
+MEGATRON_PATH=$2
+MEGATRON_PATCH_PATH=$3
+export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATCH_PATH}:$PYTHONPATH
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+if [ $ENV = dsw ]; then
+export CUDA_VISIBLE_DEVICES=6,7
+MASTER_ADDR=localhost
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+NNODES=1
+NODE_RANK=0
+GPUS_PER_NODE=2
+
+elif [ $ENV = dlc ]; then
+
+NNODES=${WORLD_SIZE}
+NODE_RANK=${RANK}
+GPUS_PER_NODE=${KUBERNETES_CONTAINER_RESOURCE_GPU}
+
+fi
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+MODEL_SIZE=$4
+BATCH_SIZE=$5
+SEQ_LEN=$6
+PAD_LEN=$7
+EXTRA_VOCAB_SIZE=$8
+PR=$9
+TP=${10}
+PP=${11}
+AC=${12}
+DO=${13}
+FL=${14}
+SP=${15}
+TE=${16}
+DATASET_PATH=${17}
+PRETRAIN_CHECKPOINT_PATH=${18}
+
+if [ $MODEL_SIZE = 7B ]; then
+
+NUM_LAYERS=32
+HIDDEN_SIZE=4096
+NUM_ATTN_HEADS=32
+INTERMEDIATE_SIZE=11008
+
+model_options=" \
+        --use-rotary-position-embeddings"
+
+elif [ $MODEL_SIZE = 13B ]; then
+
+NUM_LAYERS=40
+HIDDEN_SIZE=5120
+NUM_ATTN_HEADS=40
+INTERMEDIATE_SIZE=13696
+
+model_options=" \
+        --use-alibi-mask \
+        --position-embedding-type none"
+
+fi
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_options=" \
+		    --load $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+if [ $AC = full ]; then
+    activation_checkpoint_options=" \
+		    --recompute-method uniform \
+		    --recompute-granularity full"
+elif [ $AC = sel ]; then
+    activation_checkpoint_options=" \
+        --recompute-activations"
+elif [ $AC = none ]; then
+    activation_checkpoint_options=" \
+                    "
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+		    --fp16"
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+elif [ $PR = fp8 ]; then
+    pr_options=" \
+        --bf16
+        --fp8-hybrid \
+        --fp8-amax-compute-algo max \
+        --fp8-amax-history-len 1024 \
+        --transformer-impl transformer_engine"
+fi
+
+if [ $DO = true ]; then
+    do_options=" \
+		    --use-distributed-optimizer"
+
+elif [ $DO = false ]; then
+    do_options=" \
+                    "
+fi
+
+if [ $FL = true ]; then
+    flash_options=" \
+		    --use-flash-attn"
+
+elif [ $FL = false ]; then
+    flash_options=" \
+                    "
+fi
+
+if [ $TE = true ]; then
+    te_options=" \
+		    --transformer-impl transformer_engine"
+
+elif [ $TE = false ]; then
+    te_options=" \
+                    "
+fi
+
+if [ $SP = true ] && [ $TP -gt 1 ]; then
+    sp_options=" \
+		    --sequence-parallel"
+
+elif [ $SP = false ]; then
+    sp_options=" \
+                    "
+fi
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_options=" \
+            --load $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+
+megatron_options=" \
+        --data-path ${DATASET_PATH}
+        --micro-batch-size ${BATCH_SIZE} \
+        --num-layers ${NUM_LAYERS} \
+        --hidden-size ${HIDDEN_SIZE} \
+        --num-attention-heads ${NUM_ATTN_HEADS} \
+        --seq-length ${SEQ_LEN} \
+        --max-position-embeddings ${SEQ_LEN} \
+        --intermediate-size ${INTERMEDIATE_SIZE} \
+        --log-interval 1 \
+        --eval-interval 100 \
+        --eval-iters 10 \
+        --tensor-model-parallel-size ${TP} \
+        --pipeline-model-parallel-size ${PP} \
+        --DDP-impl local \
+        --no-load-optim \
+        --no-load-rng \
+        --num-workers 0 \
+        --seed 1234 \
+        --dataset LLama-SFT \
+        --max-padding-length ${PAD_LEN} \
+        --extra-vocab-size ${EXTRA_VOCAB_SIZE} \
+        --patch-tokenizer-type BaichuanTokenizer \
+        --swiglu \
+        --untie-embeddings-and-output-weights \
+        --disable-bias-linear
+        "
+
+run_cmd="torchrun $DISTRIBUTED_ARGS evaluate_megatron_baichuan.py
+ ${megatron_options} ${activation_checkpoint_options} ${do_options} ${pr_options} ${sp_options} ${flash_options} ${load_options} ${te_options} ${model_options}"
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
---
+++ megatron_patch/model/baichuan2/positional_embeddings.py
@@ -0,0 +1,60 @@
+# Copyright (c) 2023 Alibaba PAI and Nvidia Megatron-LM Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+
+class RotaryEmbedding(torch.nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+        self.inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        self.max_seq_len_cached = max_position_embeddings
+        t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=torch.float32)
+        freqs = torch.outer(t, self.inv_freq)
+        emb = torch.cat((freqs, freqs), dim=-1)
+        self.cos_cached = emb.cos()[None, None, :, :].to(torch.float32)
+        self.sin_cached = emb.sin()[None, None, :, :].to(torch.float32)
+    def forward(self, x, seq_len=None):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
+        if seq_len > self.max_seq_len_cached:
+            self.max_seq_len_cached = seq_len
+            t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=torch.float32)
+            freqs = torch.outer(t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            self.cos_cached = emb.cos()[None, None, :, :].to(torch.float32).to(x.device)
+            self.sin_cached = emb.sin()[None, None, :, :].to(torch.float32).to(x.device)
+        elif self.cos_cached.device != x.device:
+            self.cos_cached = self.cos_cached.to(x.device)
+            self.sin_cached = self.sin_cached.to(x.device)
+        return (
+            self.cos_cached[:, :, :seq_len, ...],
+            self.sin_cached[:, :, :seq_len, ...],
+        )
+
+
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2:]
+    return torch.cat((-x2, x1), dim=-1)
+
+
+def apply_rotary_pos_emb(q, k, cos_, sin_, position_ids):
+    cos = cos_.squeeze(1).squeeze(0)  # [seq_len, dim]
+    sin = sin_.squeeze(1).squeeze(0)  # [seq_len, dim]
+    cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
+    q_embed = (q.float() * cos) + (rotate_half(q.float()) * sin)
+    k_embed = (k.float() * cos) + (rotate_half(k.float()) * sin)
+    return q_embed.to(q.dtype), k_embed.to(k.dtype)
