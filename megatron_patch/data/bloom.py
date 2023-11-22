d0ec315e661508b89e5b02536abb832f51cf8964
Jerry Li <jerryli1981@users.noreply.github.com>
2023-11-22 08:15:26 +0800

    Fix data module remove idx=0 (#74)
    Co-authored-by: 同润 <jerry.lp@alibaba-inc.com>

--- megatron_patch/data/bloom.py
+++ megatron_patch/data/bloom.py
@@ -39,7 +39,6 @@ class BloomDataset(torch.utils.data.Dataset):
         return len(self.samples)
 
     def __getitem__(self, idx):
-        idx = 0
         raw_sample = self.samples[idx]
         return self.gpt_convert_example_to_feature(raw_sample, self.tokenizer,
                                                    self.max_seq_length)
