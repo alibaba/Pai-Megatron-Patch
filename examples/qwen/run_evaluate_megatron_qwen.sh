d008ab04771cd00224ab702674e662d9981283ee
Jerry Li <jerryli1981@users.noreply.github.com>
2023-11-22 11:21:33 +0800

    remove idx=0 in data module (#75)
    * Add Baichuan2 for 2304
    
    * Fix data module remove idx=0
    
    ---------
    
    Co-authored-by: 同润 <jerry.lp@alibaba-inc.com>

--- examples/qwen/run_evaluate_megatron_qwen.sh
+++ examples/qwen/run_evaluate_megatron_qwen.sh
@@ -9,12 +9,12 @@ MEGATRON_PATH=${MEGATRON_PATCH_PATH}/Megatron-LM-main
 export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATCH_PATH}:$PYTHONPATH
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 if [ $ENV = dsw ]; then
-export CUDA_VISIBLE_DEVICES=6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 MASTER_ADDR=localhost
 MASTER_PORT=$(shuf -n 1 -i 10000-65535)
 NNODES=1
 NODE_RANK=0
-GPUS_PER_NODE=2
+GPUS_PER_NODE=8
 
 elif [ $ENV = dlc ]; then
 
@@ -49,12 +49,25 @@ HIDDEN_SIZE=4096
 NUM_ATTN_HEADS=32
 INTERMEDIATE_SIZE=11008
 
+rope_options=""
+
 elif [ $MODEL_SIZE = 14B ]; then
 
 NUM_LAYERS=40
 HIDDEN_SIZE=5120
 NUM_ATTN_HEADS=40
 INTERMEDIATE_SIZE=13696
+
+rope_options=""
+
+elif [ $MODEL_SIZE = 72B ]; then
+NUM_LAYERS=80
+HIDDEN_SIZE=8192
+NUM_ATTN_HEADS=64
+INTERMEDIATE_SIZE=24576
+
+rope_options=" \
+		    --rotary-seq-len-interpolation-factor 4"
 
 fi
 
@@ -160,7 +173,7 @@ megatron_options=" \
         "
 
 run_cmd="torchrun $DISTRIBUTED_ARGS evaluate_megatron_qwen.py
- ${megatron_options} ${pr_options} ${load_options} ${te_options} ${activation_checkpoint_options} ${do_options} ${flash_options} ${sp_options}"
+ ${megatron_options} ${pr_options} ${load_options} ${te_options} ${activation_checkpoint_options} ${do_options} ${flash_options} ${sp_options} ${rope_options}"
 
 echo ${run_cmd}
 eval ${run_cmd}
