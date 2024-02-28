6190e87e24b2752583b9f8e38d5d6aa0ae887e0c
lwmlyy <38210876+lwmlyy@users.noreply.github.com>
2024-02-28 16:07:15 +0800

    add qwen1.5 (#139)
    Co-authored-by: jinpu.wm <jinpu.wm@alibaba-inc.com>

---
+++ examples/qwen1.5/run_pretrain_megatron_qwen.sh
@@ -0,0 +1,233 @@
+#!/bin/bash
+#sh run_pretrain_megatron_qwen.sh dsw ../../ 7B 1 8 1e-5 1e-6 2048 2048 293 bf16 1 1 sel true true true false 100000 /mnt/qwen-datasets/wudao_llamabpe_text_document /mnt/qwen-ckpts/Qwen-2-7b-hf-to-mg-tp1-pp1/ 10000000000 100000000 /mnt/output_patch_test
+set -e
+ENV=$1
+MEGATRON_PATCH_PATH=$2
+MEGATRON_PATH=${MEGATRON_PATCH_PATH}/Megatron-LM-231007
+export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATCH_PATH}:$PYTHONPATH
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+if [ $ENV = dsw ]; then
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+MASTER_ADDR=localhost
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+NNODES=1
+NODE_RANK=0
+GPUS_PER_NODE=8
+
+elif [ $ENV = dlc ]; then
+
+NNODES=${WORLD_SIZE}
+NODE_RANK=${RANK}
+GPUS_PER_NODE=${KUBERNETES_CONTAINER_RESOURCE_GPU}
+
+fi
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+MODEL_SIZE=$3
+BATCH_SIZE=$4
+GLOBAL_BATCH_SIZE=$5
+LR=$6
+MIN_LR=$7
+SEQ_LEN=$8
+PAD_LEN=$9
+EXTRA_VOCAB_SIZE=${10}
+PR=${11}
+TP=${12}
+PP=${13}
+AC=${14}
+DO=${15}
+FL=${16}
+SP=${17}
+TE=${18}
+SAVE_INTERVAL=${19}
+DATASET_PATH=${20}
+PRETRAIN_CHECKPOINT_PATH=${21}
+TRAIN_TOKENS=${22}
+WARMUP_TOKENS=${23}
+OUTPUT_BASEPATH=${24}
+
+
+if [ $MODEL_SIZE = 0.5B ]; then
+
+NUM_LAYERS=24
+HIDDEN_SIZE=1024
+NUM_ATTN_HEADS=16
+INTERMEDIATE_SIZE=2816
+
+elif [ $MODEL_SIZE = 1.8B ]; then
+
+NUM_LAYERS=24
+HIDDEN_SIZE=2048
+NUM_ATTN_HEADS=16
+INTERMEDIATE_SIZE=5504
+
+elif [ $MODEL_SIZE = 4B ]; then
+
+NUM_LAYERS=40
+HIDDEN_SIZE=2560
+NUM_ATTN_HEADS=20
+INTERMEDIATE_SIZE=6912
+
+elif [ $MODEL_SIZE = 7B ]; then
+
+NUM_LAYERS=32
+HIDDEN_SIZE=4096
+NUM_ATTN_HEADS=32
+INTERMEDIATE_SIZE=11008
+
+elif [ $MODEL_SIZE = 13B ]; then
+
+NUM_LAYERS=40
+HIDDEN_SIZE=5120
+NUM_ATTN_HEADS=40
+INTERMEDIATE_SIZE=13696
+
+elif [ $MODEL_SIZE = 72B ]; then
+
+NUM_LAYERS=80
+HIDDEN_SIZE=8192
+NUM_ATTN_HEADS=64
+INTERMEDIATE_SIZE=24576
+
+fi
+
+if [ $AC = full ]; then
+    activation_checkpoint_options=" \
+		    --recompute-method uniform \
+		    --recompute-granularity full"
+elif [ $AC = sel ]; then
+    activation_checkpoint_options=" \
+        --recompute-activations"
+elif [ $AC = none ]; then
+    activation_checkpoint_options=" \
+                    "
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+		    --fp16"
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+elif [ $PR = fp8 ]; then
+    pr_options=" \
+        --bf16
+        --fp8-hybrid \
+        --fp8-amax-compute-algo max \
+        --fp8-amax-history-len 1024 \
+        --transformer-impl transformer_engine"
+fi
+
+if [ $DO = true ]; then
+    do_options=" \
+		    --use-distributed-optimizer"
+
+elif [ $DO = false ]; then
+    do_options=" \
+                    "
+fi
+
+if [ $FL = true ]; then
+    flash_options=" \
+		    --use-flash-attn"
+
+elif [ $FL = false ]; then
+    flash_options=" \
+                    "
+fi
+
+if [ $TE = true ]; then
+    te_options=" \
+		    --transformer-impl transformer_engine"
+
+elif [ $TE = false ]; then
+    te_options=" \
+                    "
+fi
+
+if [ $SP = true ] && [ $TP -gt 1 ]; then
+    sp_options=" \
+		    --sequence-parallel"
+
+elif [ $SP = false ]; then
+    sp_options=" \
+                    "
+fi
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_options=" \
+            --load $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+TRAIN_ITERS=$(( ${TRAIN_TOKENS} / ${GLOBAL_BATCH_SIZE} / ${SEQ_LEN} ))
+LR_WARMUP_ITERS=$(( ${WARMUP_TOKENS}  / ${GLOBAL_BATCH_SIZE} / ${SEQ_LEN} ))
+LR_DECAY_ITERS=$(( ${TRAIN_TOKENS} /  ${GLOBAL_BATCH_SIZE} / ${SEQ_LEN} ))
+
+NAME="${ENV}-pretrain-megatron-gpt3-${MODEL_SIZE}-lr-${LR}-bs-${BATCH_SIZE}-seqlen-${SEQ_LEN}-pr-${PR}-tp-${TP}-pp-${PP}-ac-${AC}-do-${DO}-sp-${SP}-tt-${TRAIN_TOKENS}-wt-${WARMUP_TOKENS}"
+mkdir -p "${OUTPUT_BASEPATH}/tensorboard/"
+mkdir -p "${OUTPUT_BASEPATH}/checkpoint/"
+mkdir -p "${OUTPUT_BASEPATH}/log/"
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+TENSORBOARD_DIR="${OUTPUT_BASEPATH}/tensorboard/${NAME}_${current_time}"
+mkdir -p ${TENSORBOARD_DIR}
+
+SAVED_PRETRAIN_CHECKPOINT_PATH="${OUTPUT_BASEPATH}/checkpoint/${NAME}"
+
+megatron_options="  \
+        --save ${SAVED_PRETRAIN_CHECKPOINT_PATH} \
+        --split 98,2,0 \
+        --train-data-path ${DATASET_PATH}
+        --lr ${LR} \
+        --min-lr ${MIN_LR} \
+        --lr-decay-style linear \
+        --adam-beta1 0.9 \
+        --adam-beta2 0.95 \
+        --weight-decay 0.1 \
+        --clip-grad 1.0 \
+        --init-method-std 0.006 \
+        --lr-decay-iters ${LR_DECAY_ITERS} \
+        --lr-warmup-iters ${LR_WARMUP_ITERS} \
+        --train-iters ${TRAIN_ITERS} \
+        --micro-batch-size ${BATCH_SIZE} \
+        --global-batch-size ${GLOBAL_BATCH_SIZE} \
+        --num-layers ${NUM_LAYERS} \
+        --hidden-size ${HIDDEN_SIZE} \
+        --num-attention-heads ${NUM_ATTN_HEADS} \
+        --ffn-hidden-size ${INTERMEDIATE_SIZE} \
+        --seq-length ${SEQ_LEN} \
+        --max-position-embeddings ${SEQ_LEN} \
+        --log-interval 1 \
+        --eval-interval 10000 \
+        --eval-iters 10 \
+        --save-interval ${SAVE_INTERVAL} \
+        --tensorboard-queue-size 1 \
+        --tensorboard-dir ${TENSORBOARD_DIR} \
+        --log-timers-to-tensorboard \
+        --log-batch-size-to-tensorboard \
+        --log-validation-ppl-to-tensorboard \
+        --tensor-model-parallel-size ${TP} \
+        --pipeline-model-parallel-size ${PP} \
+        --dataset LLama-Pretrain-Idxmap \
+        --no-load-optim \
+        --no-load-rng \
+        --num-workers 8 \
+        --seed 1234 \
+        --max-padding-length ${PAD_LEN} \
+        --extra-vocab-size ${EXTRA_VOCAB_SIZE} \
+        --patch-tokenizer-type LLamaTokenizer \
+        --swiglu \
+        --normalization RMSNorm \
+        --use-llama2-rotary-position-embeddings \
+        --position-embedding-type rope \
+        --untie-embeddings-and-output-weights \
+        --rotary-base 1000000 \
+        --rotary-scale-factor 1 \
+        "
+
+run_cmd="torchrun $DISTRIBUTED_ARGS ../llama2/pretrain_megatron_llama.py
+ ${megatron_options} ${pr_options} ${load_options} ${te_options} ${activation_checkpoint_options} ${do_options} ${flash_options} ${sp_options}"
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
---
+++ examples/qwen1.5/run_text_generation_megatron_qwen.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+# bash run_text_generation_megatron_qwen.sh dsw ../../ /mnt/workspace/latest/qianwen/models--Qwen--Qwen2-beta-1_8B-Chat-hf2mg21 1.8B 2 1 1024 80 0 fp16 0 512 512 /mnt/qwen-datasets/gen.jsonl /mnt/qwen-datasets/cn_output.txt 0.85 1 1
+set -e
+ENV=$1
+export CUDA_VISIBLE_DEVICES=0,1,2,3
+MASTER_ADDR=localhost
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+GPUS_PER_NODE=4
+NNODES=1
+NODE_RANK=0
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+MEGATRON_PATCH_PATH=$2
+MEGATRON_PATH=${MEGATRON_PATCH_PATH}/Megatron-LM-231007
+export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATCH_PATH}:$PYTHONPATH
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+CHECKPOINT_PATH=$3
+MODEL_SIZE=$4  #7B, 13B, 70B
+TP=$5
+BS=$6
+SEQ_LEN=$7
+PAD_LEN=$8
+EXTRA_VOCAB_SIZE=$9
+PR=${10}
+TOP_K=${11}
+INPUT_SEQ_LEN=${12}
+OUTPUT_SEQ_LEN=${13}
+INPUT_FILE=${14}
+OUTPUT_FILE=${15}
+TOP_P=${16}
+TEMPERATURE=${17}
+# set this penalty between 1.1 and 1.5 to reduce repetition, default is 1.2
+REPETITION_PENALTY=${18}
+
+if [ $MODEL_SIZE = 0.5B ]; then
+
+NUM_LAYERS=24
+HIDDEN_SIZE=1024
+NUM_ATTN_HEADS=16
+INTERMEDIATE_SIZE=2816
+
+elif [ $MODEL_SIZE = 1.8B ]; then
+
+NUM_LAYERS=24
+HIDDEN_SIZE=2048
+NUM_ATTN_HEADS=16
+INTERMEDIATE_SIZE=5504
+
+elif [ $MODEL_SIZE = 4B ]; then
+
+NUM_LAYERS=40
+HIDDEN_SIZE=2560
+NUM_ATTN_HEADS=20
+INTERMEDIATE_SIZE=6912
+
+elif [ $MODEL_SIZE = 7B ]; then
+
+NUM_LAYERS=32
+HIDDEN_SIZE=4096
+NUM_ATTN_HEADS=32
+INTERMEDIATE_SIZE=11008
+
+elif [ $MODEL_SIZE = 13B ]; then
+
+NUM_LAYERS=40
+HIDDEN_SIZE=5120
+NUM_ATTN_HEADS=40
+INTERMEDIATE_SIZE=13696
+
+elif [ $MODEL_SIZE = 72B ]; then
+
+NUM_LAYERS=80
+HIDDEN_SIZE=8192
+NUM_ATTN_HEADS=64
+INTERMEDIATE_SIZE=24576
+
+fi
+
+if [ $CHECKPOINT_PATH != none ]; then
+    load_options=" \
+		    --load $CHECKPOINT_PATH"
+fi
+
+if [ $INPUT_FILE = none ]; then
+    input_options=" \
+		               "
+else
+    input_options=" \
+        --text-generate-output-file ${OUTPUT_FILE}\
+        --text-generate-input-file ${INPUT_FILE} \
+        "
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+		    --fp16"
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+fi
+
+rapidformer_options="  \
+        --micro-batch-size ${BS} \
+        --num-layers ${NUM_LAYERS}  \
+        --hidden-size ${HIDDEN_SIZE}  \
+        --num-attention-heads ${NUM_ATTN_HEADS}  \
+        --ffn-hidden-size ${INTERMEDIATE_SIZE} \
+        --seq-length ${SEQ_LEN} \
+        --max-position-embeddings ${SEQ_LEN} \
+        --tensor-model-parallel-size ${TP} \
+        --pipeline-model-parallel-size 1 \
+        --no-load-optim \
+        --no-load-rng \
+        --top-p ${TOP_P} \
+        --temperature ${TEMPERATURE}  \
+        --top-k ${TOP_K} \
+        --input-len ${INPUT_SEQ_LEN} \
+        --out-seq-length ${OUTPUT_SEQ_LEN}  \
+        --extra-vocab-size ${EXTRA_VOCAB_SIZE} \
+        --max-padding-length ${PAD_LEN} \
+        --use-distributed-optimizer \
+        --swiglu \
+        --use-llama2-rotary-position-embeddings \
+        --position-embedding-type rope \
+        --untie-embeddings-and-output-weights \
+        --patch-tokenizer-type LLamaTokenizer \
+        --normalization RMSNorm \
+        --repetition-penalty ${REPETITION_PENALTY} \
+        --rotary-base 1000000 \
+        --rotary-scale-factor 1 \
+    "
+
+run_cmd="torchrun $DISTRIBUTED_ARGS ../llama2/generate_text_megatron_llama.py
+ ${rapidformer_options} ${load_options} ${input_options} ${pr_options}"
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
