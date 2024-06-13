0f5387549aa924649d7e6d8e719a148bcc464535
Jerry Li <jerryli1981@users.noreply.github.com>
2024-06-13 16:00:08 +0800

    Add Qwen2 Dense Models Forward Loss Evalution Checking (#259)
    Co-authored-by: 同润 <jerry.lp@alibaba-inc.com>

---
+++ examples/qwen2/run_evaluate_huggingface_qwen.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+# sh run_evaluate_huggingface_qwen.sh 0.5B 1 256 256 bf16 /mnt/qwen-datasets/alpaca_zh-qwen-valid.json /mnt/qwen-ckpts/Qwen2-0.5B
+
+set -e
+CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+MEGATRON_PATH=$( dirname $( dirname ${CURRENT_DIR}))
+export PYTHONPATH=$PYTHONPATH:${MEGATRON_PATH}:${MEGATRON_PATH}/Megatron-LM-240612
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+export CUDA_VISIBLE_DEVICES=6
+MASTER_ADDR=localhost
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+NNODES=1
+NODE_RANK=0
+GPUS_PER_NODE=1
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+
+MODEL_SIZE=$1
+BATCH_SIZE=$2
+SEQ_LEN=$3
+PAD_LEN=$4
+PR=$5
+DATASET_PATH=$6
+PRETRAIN_CHECKPOINT_PATH=$7
+
+
+if [ $MODEL_SIZE = 0.5B ]; then
+
+HIDDEN_SIZE=896
+INTERMEDIATE_SIZE=4864
+MAX_POSITION_EMBEDDINGS=131072
+MAX_WINDOW_LAYERS=24
+NUM_ATTENTION_HEADS=14
+NUM_HIDDEN_LAYERS=24
+NUM_KEY_VALUE_HEADS=2
+RMS_NORM_EPS=1e-6
+ROPE_THETA=1000000
+SLIDING_WINDOW=131072
+EXTRA_VOCAB_SIZE=293
+
+moe_options=" \
+            "
+
+elif [ $MODEL_SIZE = 1.5B ]; then
+
+HIDDEN_SIZE=1536
+INTERMEDIATE_SIZE=8960
+MAX_POSITION_EMBEDDINGS=131072
+MAX_WINDOW_LAYERS=28
+NUM_ATTENTION_HEADS=12
+NUM_HIDDEN_LAYERS=28
+NUM_KEY_VALUE_HEADS=2
+RMS_NORM_EPS=1e-6
+ROPE_THETA=1000000
+SLIDING_WINDOW=131072
+EXTRA_VOCAB_SIZE=293
+
+moe_options=" \
+            "
+
+elif [ $MODEL_SIZE = 7B ]; then
+
+HIDDEN_SIZE=3584
+INTERMEDIATE_SIZE=18944
+MAX_POSITION_EMBEDDINGS=131072
+MAX_WINDOW_LAYERS=28
+NUM_ATTENTION_HEADS=28
+NUM_HIDDEN_LAYERS=28
+NUM_KEY_VALUE_HEADS=4
+RMS_NORM_EPS=1e-6
+ROPE_THETA=1000000
+SLIDING_WINDOW=131072
+EXTRA_VOCAB_SIZE=293
+
+moe_options=" \
+            "
+
+elif [ $MODEL_SIZE = 72B ]; then
+
+HIDDEN_SIZE=8192
+INTERMEDIATE_SIZE=29568
+MAX_POSITION_EMBEDDINGS=131072
+MAX_WINDOW_LAYERS=80
+NUM_ATTENTION_HEADS=64
+NUM_HIDDEN_LAYERS=80
+NUM_KEY_VALUE_HEADS=8
+RMS_NORM_EPS=1e-5
+ROPE_THETA=1000000
+SLIDING_WINDOW=131072
+EXTRA_VOCAB_SIZE=421
+
+moe_options=" \
+            "
+
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+            --fp16"
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+fi
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_options=" \
+            --load $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+
+megatron_options=" \
+        --transformer-type huggingface \
+        --valid-data-path ${DATASET_PATH}
+        --micro-batch-size ${BATCH_SIZE} \
+        --num-layers ${NUM_HIDDEN_LAYERS} \
+        --hidden-size ${HIDDEN_SIZE} \
+        --num-attention-heads ${NUM_ATTENTION_HEADS} \
+        --seq-length ${SEQ_LEN} \
+        --max-position-embeddings ${SEQ_LEN} \
+        --log-interval 1 \
+        --eval-interval 100 \
+        --eval-iters 10 \
+        --tensor-model-parallel-size 1 \
+        --pipeline-model-parallel-size 1 \
+        --no-load-optim \
+        --num-workers 0 \
+        --dataset LLama-SFT \
+        --use-distributed-optimizer \
+        --max-padding-length ${PAD_LEN} \
+        --extra-vocab-size ${EXTRA_VOCAB_SIZE} \
+        --patch-tokenizer-type LLamaTokenizer
+        "
+
+run_cmd="torchrun $DISTRIBUTED_ARGS evaluate_huggingface_qwen.py
+ ${megatron_options} ${pr_options} ${load_options}"
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
