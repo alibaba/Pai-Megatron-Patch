51d49f0785144413317cbe88b7d0469ba9991b25
Jerry Li <jerryli1981@users.noreply.github.com>
2024-11-21 14:15:16 +0800

    Add LLaVa mcore implementation (#384)
    Co-authored-by: 同润 <jerry.lp@alibaba-inc.com>

---
+++ examples/llava_mcore/run_mcore_llava.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+set -e
+CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+MEGATRON_PATH=$( dirname $( dirname ${CURRENT_DIR}))
+export PYTHONPATH=${MEGATRON_PATH}:${MEGATRON_PATH}/Megatron-LM-241113:$PYTHONPATH
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export NVTE_APPLY_QK_LAYER_SCALING=0
+export NVTE_ALLOW_NONDETERMINISTIC_ALGO=1
+
+ENV=$1
+if [ $ENV = dsw ]; then
+    export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+    MASTER_ADDR=localhost
+    MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+    NNODES=1
+    NODE_RANK=0
+    GPUS_PER_NODE=8
+elif [ $ENV = dlc ]; then
+    NNODES=${WORLD_SIZE}
+    NODE_RANK=${RANK}
+    GPUS_PER_NODE=${KUBERNETES_CONTAINER_RESOURCE_GPU}
+fi
+
+
+### BASE CONFIG ###
+MODEL_SIZE=$2
+BATCH_SIZE=$3
+GLOBAL_BATCH_SIZE=$4
+LR=$5
+MIN_LR=$6
+SEQ_LEN=$7
+DECODER_SEQ_LEN=$8
+PR=${9}
+### BASE CONFIG ###
+
+### PARALLEL / BOOL OPTION ###
+TP=${10}
+PP=${11}
+CP=${12}
+DO=${13}
+FL=${14}
+### PARALLEL / BOOL OPTION ###
+
+### OTHERS ###
+AC=${15}
+OPTIMIZER_OFFLOAD=${16}
+SAVE_INTERVAL=${17}
+DATASET_PATH=${18}
+VALID_DATASET_PATH=${19}
+PRETRAIN_CHECKPOINT_PATH=${20}
+
+TRAIN_ITERS=${21}
+LR_WARMUP_ITERS=${22}
+###############################
+
+OUTPUT_BASEPATH=${23}
+### OTHERS ###
+
+if [ $FL = true ]; then
+    export NVTE_FLASH_ATTN=1 NVTE_FUSED_ATTN=0
+    flash_options=" \
+          --use-flash-attn"
+
+elif [ $FL = false ]; then
+    export NVTE_FLASH_ATTN=0 NVTE_FUSED_ATTN=1
+    flash_options=""
+fi
+
+if [ $MODEL_SIZE = 7B ]; then
+
+NUM_LAYERS=32
+HIDDEN_SIZE=4096
+NUM_ATTN_HEADS=32
+INTERMEDIATE_SIZE=14336
+MAX_POSITION_EMBEDDINGS=4096
+NUM_KEY_VALUE_HEADS=8
+
+gqa_options=" \
+		    --group-query-attention \
+		    --num-query-groups ${NUM_KEY_VALUE_HEADS}"
+
+language_model_type="mistral_7b"
+
+fi
+
+if [ $AC = full ]; then
+    _check=$(( ($NUM_LAYERS / $PP) % ${MP_AC_LAYERS} ))
+    if [ $_check != 0 ]; then
+        echo "the num layers per pp rank must be a multiple of the recompute layers."
+        exit -1
+    fi
+    activation_checkpoint_options=" \
+		    --recompute-method uniform \
+            --recompute-num-layers ${MP_AC_LAYERS} \
+		    --recompute-granularity full"
+elif [ $AC = sel ]; then
+    activation_checkpoint_options=" \
+        --recompute-activations"
+elif [ $AC = none ]; then
+    activation_checkpoint_options=" \
+    "
+elif [ $AC = offload ]; then
+    activation_checkpoint_options=" \
+		    --cpu-offloading \
+		    --cpu-offloading-num-layers ${MP_AC_LAYERS}"
+    if [ $TP_COMM_OVERLAP -eq 1 ]; then
+        echo "Disable --overlap-grad-reduce and --overlap-param-gather when cpu offloading is on..."
+        comm_overlap_option="\
+            --tp-comm-overlap"
+    else
+        echo "Disable --overlap-grad-reduce and --overlap-param-gather when cpu offloading is on..."
+        comm_overlap_option=""
+    fi
+fi
+
+if [ $PR = fp16 ]; then
+    pr_options=" \
+		    --fp16 \
+            --apply-query-key-layer-scaling"
+    export NVTE_APPLY_QK_LAYER_SCALING=1
+elif [ $PR = bf16 ]; then
+    pr_options=" \
+        --bf16"
+elif [ $PR = fp8 ]; then
+    pr_options=" \
+        --bf16 \
+        --fp8-format hybrid \
+        --fp8-amax-compute-algo max \
+        --fp8-amax-history-len 1024"
+fi
+
+if [ $OPTIMIZER_OFFLOAD != false ] && [ $DO = false ]; then
+    echo "Offload optimizer is valid only if \$DO=true"
+    DO=true
+fi
+
+if [ $DO = true ]; then
+    do_options=" \
+		    --use-distributed-optimizer"
+
+elif [ $DO = false ]; then
+    do_options=" \
+                    "
+fi
+
+
+if [ $PRETRAIN_CHECKPOINT_PATH != none ]; then
+    load_options=" \
+            --pretrained-checkpoint $PRETRAIN_CHECKPOINT_PATH"
+fi
+
+if [ $OPTIMIZER_OFFLOAD = 'static' ]; then
+    offload_option=" \
+        --optimizer hybridadam \
+        --optimizer-offload-policy static \
+        --optimizer-offload-fraction 1.0"
+elif [ $OPTIMIZER_OFFLOAD = 'auto' ]; then
+    offload_option=" \
+        --optimizer hybridadam \
+        --optimizer-offload-policy auto"
+else
+    offload_option=""
+fi
+
+
+LR_DECAY_ITERS=$(( ${TRAIN_ITERS} - ${LR_WARMUP_ITERS}))
+PREFIX="finetune-mcore-llava-${MODEL_SIZE}-lr-${LR}-minlr-${MIN_LR}-bs-${BATCH_SIZE}-gbs-${GLOBAL_BATCH_SIZE}-seqlen-${SEQ_LEN}"
+
+
+##### Prepare logdirs #######
+NAME="${PREFIX}-pr-${PR}-tp-${TP}-pp-${PP}-cp-${CP}-ac-${AC}-do-${DO}-sp-${SP}-ti-${TRAIN_ITERS}-wi-${LR_WARMUP_ITERS}"
+mkdir -p "${OUTPUT_BASEPATH}/tensorboard/"
+mkdir -p "${OUTPUT_BASEPATH}/checkpoint/"
+mkdir -p "${OUTPUT_BASEPATH}/log/"
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+TENSORBOARD_DIR="${OUTPUT_BASEPATH}/tensorboard/${NAME}_${current_time}"
+mkdir -p ${TENSORBOARD_DIR}
+SAVED_PRETRAIN_CHECKPOINT_PATH="${OUTPUT_BASEPATH}/checkpoint/${NAME}"
+
+mkdir -p ${SAVED_PRETRAIN_CHECKPOINT_PATH}
+find -L ${PRETRAIN_CHECKPOINT_PATH} -maxdepth 1 -type f -name "*.json" -print0 | xargs -0 cp -t ${SAVED_PRETRAIN_CHECKPOINT_PATH}
+
+
+megatron_options="  \
+        --train-data-path ${DATASET_PATH} \
+        --valid-data-path ${VALID_DATASET_PATH} \
+        --split 100,0,0 \
+        --transformer-impl transformer_engine \
+        --save ${SAVED_PRETRAIN_CHECKPOINT_PATH} \
+        --lr ${LR} \
+        --min-lr ${MIN_LR} \
+        --lr-decay-style cosine \
+        --weight-decay 0.1 \
+        --adam-beta1 0.9 \
+        --adam-beta2 0.95 \
+        --clip-grad 1.0 \
+        --init-method-std 0.014 \
+        --attention-dropout 0.0 \
+        --hidden-dropout 0.1 \
+        --lr-decay-iters ${LR_DECAY_ITERS} \
+        --lr-warmup-iters ${LR_WARMUP_ITERS} \
+        --train-iters ${TRAIN_ITERS} \
+        --micro-batch-size ${BATCH_SIZE} \
+        --global-batch-size ${GLOBAL_BATCH_SIZE} \
+        --num-layers ${NUM_LAYERS} \
+        --hidden-size ${HIDDEN_SIZE} \
+        --num-attention-heads ${NUM_ATTN_HEADS} \
+        --ffn-hidden-size ${INTERMEDIATE_SIZE} \
+        --seq-length ${SEQ_LEN} \
+        --decoder-seq-length ${DECODER_SEQ_LEN} \
+        --max-position-embeddings ${MAX_POSITION_EMBEDDINGS} \
+        --log-interval 1 \
+        --log-throughput \
+        --eval-interval 10000 \
+        --eval-iters 10 \
+        --save-interval ${SAVE_INTERVAL} \
+        --tensorboard-queue-size 1 \
+        --tensorboard-dir ${TENSORBOARD_DIR} \
+        --log-timers-to-tensorboard \
+        --log-validation-ppl-to-tensorboard \
+        --tensor-model-parallel-size ${TP} \
+        --pipeline-model-parallel-size ${PP} \
+        --context-parallel-size ${CP} \
+        --no-load-optim \
+        --no-load-rng \
+        --num-workers 2 \
+        --patch-tokenizer-type LLama3Tokenizer \
+        --swiglu \
+        --normalization RMSNorm \
+        --norm-epsilon 1e-05 \
+        --use-rotary-position-embeddings \
+        --position-embedding-type rope \
+        --untie-embeddings-and-output-weights \
+        --disable-bias-linear \
+        --rotary-percent 1.0 \
+        --rotary-base 1000000 \
+        --no-save-optim \
+        --allow-missing-vision-projection-checkpoint \
+        --disable-vision-class-token \
+        --dataloader-type external \
+        --freeze-LM \
+        --freeze-ViT \
+        --patch-dim 14 \
+        --img-h 336 \
+        --img-w 336 \
+        --eod-mask-loss \
+        --language-model-type ${language_model_type} \
+        --tokenizer-type MultimodalTokenizer \
+        --tokenizer-model ${PRETRAIN_CHECKPOINT_PATH} \
+        --tokenizer-prompt-format mistral \
+        --apply-layernorm-1p \
+        --attention-softmax-in-fp32 \
+        --no-masked-softmax-fusion \
+        --eod-mask-loss
+        "
+
+
+DISTRIBUTED_ARGS="--nproc_per_node $GPUS_PER_NODE --nnodes $NNODES --node_rank $NODE_RANK --master_addr $MASTER_ADDR --master_port $MASTER_PORT"
+run_cmd="torchrun $DISTRIBUTED_ARGS pretrain_llava.py
+ ${megatron_options} ${pr_options} ${load_options} ${activation_checkpoint_options} \
+ ${do_options} ${gqa_options} ${offload_option} ${vp_options} ${flash_options}"
+
+echo ${run_cmd}
+eval ${run_cmd}
+set +x
