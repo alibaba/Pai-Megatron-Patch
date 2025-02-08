48343f8467dbdb3dce03c69b6d6b63490fb7aa68
lostkevin <46404040+lostkevin@users.noreply.github.com>
2025-02-08 21:34:24 +0800

    add per_seq loss for dsk2; fix issues (#457)
    * fix checkpointing issue
    
    * add missed file and bugfix
    
    * add per_seq loss for dsk2; fix issues

--- examples/deepseek_v2/pretrain_deepseek.py
+++ examples/deepseek_v2/pretrain_deepseek.py
@@ -1,5 +1,16 @@
-# Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
-"""Pretrain GPT."""
+# Copyright (c) 2023 Alibaba PAI and Nvidia Megatron-LM Team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
 from functools import partial
@@ -92,18 +103,28 @@ def get_batch(data_iterator):
         if args.train_mode == "pretrain":
             raise ValueError('The LLama-SFT-Raw dataset should only be used for finetuning!')
         # get batches based on the TP rank you are on
-        batch = get_batch_on_this_tp_rank_original(data_iterator)
+        batch = get_batch_on_this_tp_rank_original(data_iterator, per_seq_average=True)
         # slice batch along sequence dimension for context parallelism
+        num_seqs = batch.pop('num_seqs')
         batch = get_batch_on_this_cp_rank(batch)
 
-        return tuple([*batch.values(), None])
+        return (
+            batch['tokens'],
+            batch['labels'],
+            batch['loss_mask'],
+            batch['attention_mask'],
+            batch['position_ids'],
+            num_seqs,
+            None
+        )
     elif "-Idxmap" in args.dataset:
         # get batches based on the TP rank you are on
         if args.train_mode == "pretrain":
             batch = get_batch_on_this_tp_rank(data_iterator)
+            
         else:
             batch = get_batch_on_this_tp_rank_idxmap_sft(data_iterator, per_seq_average=True)
-
+        
         packed_seq_params = None
         if args.reset_position_ids:
             # sequence-packing, build cu_seqlens
@@ -122,21 +143,27 @@ def get_batch(data_iterator):
                     cu_seqlens_kv=cu_seqlens,
                     qkv_format='thd'
                 )
-
+        
         if packed_seq_params is not None and args.context_parallel_size > 1:
             raise ValueError('Sequence Packing is not supported when CP>1 !')
         # slice batch along sequence dimension for context parallelism
+        num_seqs = batch.pop('num_seqs', None)
         batch = get_batch_on_this_cp_rank(batch)
 
-        if args.train_mode == "pretrain":
-            return tuple([*batch.values(), None, packed_seq_params])
-        else:
-            return tuple([*batch.values(), packed_seq_params])
+        return (
+            batch['tokens'],
+            batch['labels'],
+            batch['loss_mask'],
+            batch['attention_mask'],
+            batch['position_ids'],
+            num_seqs,
+            packed_seq_params
+        )
     else:
         raise ValueError("please set correct --dataset ")
 
 
-def loss_func(loss_mask: torch.Tensor, output_tensor: torch.Tensor):
+def loss_func(loss_mask: torch.Tensor, num_seqs: torch.Tensor, output_tensor: torch.Tensor):
     """Loss function.
 
     Args:
@@ -147,27 +174,29 @@ def loss_func(loss_mask: torch.Tensor, output_tensor: torch.Tensor):
 
     losses = output_tensor.float()
     loss_mask = loss_mask.view(-1).float()
+
+    loss = torch.stack([torch.sum(losses.view(-1) * loss_mask), loss_mask.sum()])
     if args.context_parallel_size > 1:
-        loss = torch.cat(
-            [torch.sum(losses.view(-1) * loss_mask).view(1), loss_mask.sum().view(1)]
-        )
         torch.distributed.all_reduce(loss, group=mpu.get_context_parallel_group())
-        loss = loss[0] / loss[1]
-    else:
-        loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()
 
     # Check individual rank losses are not NaN prior to DP all-reduce.
     if args.check_for_nan_in_loss_and_grad:
         global_rank = torch.distributed.get_rank()
-        assert not loss.isnan(), (
+        assert not loss.isnan().any(), (
             f"Rank {global_rank}: found NaN in local forward loss calculation. "
             f"Device: {torch.cuda.current_device()}, node: {os.uname()[1]}"
         )
 
-    # Reduce loss for logging.
-    averaged_loss = average_losses_across_data_parallel_group([loss])
+    averaged_loss = average_losses_across_data_parallel_group(loss)
+    averaged_loss = averaged_loss[0] / averaged_loss[1]
 
-    return loss * args.context_parallel_size, {"lm loss": averaged_loss[0]}
+    # NOTE: The grad will be scaled down by CP size later, should not remove this multilication factor
+    # LINK: https://github.com/NVIDIA/Megatron-LM/issues/906
+    # The issue is solved since 0926
+
+    if num_seqs is None:
+        return loss[0] * args.context_parallel_size, {"lm loss": averaged_loss}
+    return loss[0] * args.context_parallel_size, num_seqs.sum(), {"lm loss": averaged_loss}
 
 
 def forward_step(data_iterator, model: GPTModel):
@@ -182,11 +211,11 @@ def forward_step(data_iterator, model: GPTModel):
 
     # Get the batch.
     timers("batch-generator", log_level=2).start()
-    tokens, labels, loss_mask, attention_mask, position_ids, _, packed_seq_params = get_batch(data_iterator)
+    tokens, labels, loss_mask, attention_mask, position_ids, num_seqs, packed_seq_params = get_batch(data_iterator)
     timers("batch-generator").stop()
     output_tensor = model(tokens, position_ids, attention_mask, labels=labels, packed_seq_params=packed_seq_params)
 
-    return output_tensor, partial(loss_func, loss_mask)
+    return output_tensor, partial(loss_func, loss_mask, num_seqs)
 
 
 def is_dataset_built_on_rank():
@@ -237,7 +266,6 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
         # therefore we need to double the seqlen
         if args.train_mode != "pretrain":
             config.sequence_length = config.sequence_length * 2
-
         if config.mock:
             dataset_type = MockGPTDataset
         else:
