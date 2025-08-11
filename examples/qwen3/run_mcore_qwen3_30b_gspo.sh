8d010127c04d7ce92d2508d2bf370150360853f4
Peng Li <jerryli1981@gmail.com>
2025-08-11 06:19:10 +0000

    Support all Mcore Models GSPO Reinforcement Training via ChatLearn

--- examples/qwen3/run_mcore_qwen3_30b_gspo.sh
+++ examples/qwen3/run_mcore_qwen3_30b_gspo.sh
@@ -31,25 +31,25 @@ export RAY_DEDUP_LOGS=0
 export VLLM_USE_RAY_SPMD_WORKER=1
 export VLLM_USE_RAY_COMPILED_DAG=1
 
-hf_ckpt_path=/mnt/data/ckpts/huggingface/DeepSeek-V3-0324-BF16
-mcore_ckpt_path=/mnt/data/ckpts/mcore/DeepSeek-V3-0324-BF16-to-mcore
-exp_name="test_dsv3_671b_grpo"
+hf_ckpt_path=/mnt/data/ckpts/huggingface/Qwen3-30B-A3B
+mcore_ckpt_path=/mnt/data/ckpts/mcore/Qwen3-30B-A3B-to-mcore
+exp_name="test_qwen3_30b"
 export output_dir=${MEGATRON_PATCH_PATH}/output/${exp_name}
 mkdir -p $output_dir/
 export log_dir=${output_dir}/logs
 mkdir -p $log_dir
 log_file=$log_dir/${exp_name}_rank${RANK}.log
 
-python ../qwen3/entrypoint.py grpo --config-file ../qwen3/configs/grpo_megatron.yaml \
+python entrypoint.py grpo --config-file configs/grpo_megatron.yaml \
         runtime_args.exp_name=${exp_name} \
         runtime_args.log_args_dict.enable_tensorboard=True \
         runtime_args.train_backend=megatron \
         runtime_args.data_path=/mnt/data/datasets/MATH-lighteval/train.json \
         runtime_args.eval_data_path=/mnt/data/datasets/MATH-lighteval/test.json \
         runtime_args.output_dir=${output_dir}\
-        runtime_args.num_episode=50 \
+        runtime_args.num_episode=100 \
         runtime_args.sample_per_episode=1024 \
-        runtime_args.train_global_batch_size=32 \
+        runtime_args.train_global_batch_size=256 \
         runtime_args.train_micro_batch_size=1 \
         runtime_args.save_episode_interval=1000000 \
         runtime_args.log_args_dict.enable_tensorboard=true \
@@ -57,32 +57,30 @@ python ../qwen3/entrypoint.py grpo --config-file ../qwen3/configs/grpo_megatron.
         runtime_args.eval_episode_interval=1 \
         runtime_args.enable_eval_before_training=false \
         models.policy_trainer.num_gpu=${num_device} \
-        models.policy_trainer.packing=true \
-        models.policy_trainer.max_token_in_packing=8192 \
+        models.policy_trainer.packing=false \
         models.policy_trainer.bf16=true \
         models.policy_trainer.sequence_parallel=true \
         models.policy_trainer.use_distributed_optimizer=true \
-        models.policy_trainer.recompute_granularity=null \
+        models.policy_trainer.recompute_granularity='selective' \
         models.policy_trainer.seq_length=2048 \
-        models.policy_trainer.tensor_model_parallel_size=1 \
-        models.policy_trainer.pipeline_model_parallel_size=16 \
-        models.policy_trainer.expert_model_parallel_size=16 \
-        models.policy_trainer.expert_tensor_parallel_size=1 \
-        models.policy_trainer.decoder_first_pipeline_num_layers=3 \
-        models.policy_trainer.decoder_last_pipeline_num_layers=2 \
-        models.policy_trainer.generation_batch_size=128 \
+        models.policy_trainer.tensor_model_parallel_size=4 \
+        models.policy_trainer.pipeline_model_parallel_size=2 \
+        models.policy_trainer.expert_tensor_parallel_size=4 \
+        models.policy_trainer.expert_model_parallel_size=1 \
+        models.policy_trainer.generation_batch_size=32 \
         models.policy_trainer.load=${mcore_ckpt_path} \
         models.policy_trainer.optimizer.lr=2e-6 \
         models.policy_trainer.optimizer.min_lr=2e-6 \
-        models.policy_trainer.pos_clip_ratio=0.2 \
-        models.policy_trainer.neg_clip_ratio=0.2 \
+        models.policy_trainer.pos_clip_ratio=1e-3 \
+        models.policy_trainer.neg_clip_ratio=1e-3 \
+        models.policy_trainer.use_group_sequence_policy=true \
         models.reward.generation_batch_size=128 \
         models.policy.load=${hf_ckpt_path} \
         models.policy.generation_batch_size=128 \
-        models.policy.tensor_model_parallel_size=32 \
+        models.policy.tensor_model_parallel_size=4 \
         models.policy.seq_length=2048 \
         models.policy.max_seq_len_to_capture=2348 \
         models.policy.num_inference_per_prompt=32 \
         models.policy.gpu_memory_utilization=0.75 \
-        models.policy.enable_thinking=False \
+        models.policy.enable_thinking=True \
         2>&1 | tee ${log_file} ; exit ${PIPESTATUS[0]}
