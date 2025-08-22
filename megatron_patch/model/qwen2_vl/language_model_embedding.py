fed9fa2021565aa655fba6174b866bfd82819cbe
Ziang Wu <97337387+ZiangWu-77@users.noreply.github.com>
2025-08-22 10:21:28 +0800

    Fix the combination of vision embeds and text embeds in qwen2vl (#649)
    * Update language_model_embedding.py
    
    avoid wrong masked_scatter
    
    * Update language_model_embedding.py

--- megatron_patch/model/qwen2_vl/language_model_embedding.py
+++ megatron_patch/model/qwen2_vl/language_model_embedding.py
@@ -141,10 +141,14 @@ class LanguageModelEmbedding(MegatronModule):
         if self.config.sequence_parallel:
             if not self.reduce_scatter_embeddings:
                 embeddings = embeddings.clone()
+                embeddings = embeddings.transpose(0, 1).contiguous()
                 if image_embeds is not None:
-                    embeddings[image_input_mask] = image_embeds.to(embeddings.device, embeddings.dtype)
+                    image_input_mask = image_input_mask.transpose(0,1).unsqueeze(-1).expand_as(embeddings)
+                    embeddings = embeddings.masked_scatter(image_input_mask, image_embeds.to(embeddings.device, embeddings.dtype))
                 if video_embeds is not None:
-                    embeddings[video_input_mask] = video_embeds.to(embeddings.device, embeddings.dtype)
+                    video_input_mask = video_input_mask.transpose(0,1).unsqueeze(-1).expand_as(embeddings)
+                    embeddings = embeddings.masked_scatter(video_input_mask, video_embeds.to(embeddings.device, embeddings.dtype))
+                embeddings = embeddings.transpose(0, 1).contiguous()
                 embeddings = tensor_parallel.scatter_to_sequence_parallel_region(embeddings)
             # `scatter_to_sequence_parallel_region` returns a view, which prevents
             # the original tensor from being garbage collected. Clone to facilitate GC.
@@ -155,10 +159,16 @@ class LanguageModelEmbedding(MegatronModule):
                 embeddings = self.embedding_dropout(embeddings)
         else:
             embeddings = embeddings.clone()
+            if not self.reduce_scatter_embeddings:
+                embeddings = embeddings.transpose(0, 1).contiguous()
             if image_embeds is not None:
-                embeddings[image_input_mask] = image_embeds.to(embeddings.device, embeddings.dtype)
+                image_input_mask = image_input_mask.transpose(0,1).unsqueeze(-1).expand_as(embeddings)
+                embeddings = embeddings.masked_scatter(image_input_mask, image_embeds.to(embeddings.device, embeddings.dtype))
             if video_embeds is not None:
-                embeddings[video_input_mask] = video_embeds.to(embeddings.device, embeddings.dtype)
+                video_input_mask = video_input_mask.transpose(0,1).unsqueeze(-1).expand_as(embeddings)
+                embeddings = embeddings.masked_scatter(video_input_mask, video_embeds.to(embeddings.device, embeddings.dtype))
+            if not self.reduce_scatter_embeddings:
+                embeddings = embeddings.transpose(0, 1).contiguous()
             embeddings = self.embedding_dropout(embeddings)
 
         return embeddings
